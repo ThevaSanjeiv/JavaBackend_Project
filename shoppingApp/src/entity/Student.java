befd6e4aff2c7edd5a1f8c96161478b8fd1960ab
Jupiter-0109 <jupiter01092004@gmail.com>
2025-07-30 12:05:32 +0530

    push from jupi

--- shoppingApp/src/entity/Student.java
+++ shoppingApp/src/entity/Student.java
@@ -4,8 +4,11 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("students")
+		System.out.println("students");
 			System.out.println("java");
+			System.out.println("hello lead from jupi");
+			
+			
 		
 	}
 
